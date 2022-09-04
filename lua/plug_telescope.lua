a73bf2bee85ef305cc3d90332de95c8559812e89
YS <kerol2r20@gmail.com>
2022-09-04 18:46:11 +0800

    Fix: wrong key mapping used by Telescope

--- lua/plug_telescope.lua
+++ lua/plug_telescope.lua
@@ -1,10 +1,13 @@
 -- This file define telescope plugin configurations
 
 function telescope_keymapping()
+    -- file about pickers
     vim.keymap.set('n', '<Leader>ff', [[<CMD>lua require('telescope.builtin').find_files()<CR>]])
+
+    -- lsp about pickers
     vim.keymap.set('n', '<Leader>sr', [[<CMD>lua require('telescope.builtin').lsp_references()<CR>]])
-    vim.keymap.set('n', '<Leader>si', [[<CMD>lua require('telescope.builtin').lsp_references()<CR>]])
-    vim.keymap.set('n', '<Leader>so', [[<CMD>lua require('telescope.builtin').lsp_references()<CR>]])
+    vim.keymap.set('n', '<Leader>si', [[<CMD>lua require('telescope.builtin').lsp_incoming_calls()<CR>]])
+    vim.keymap.set('n', '<Leader>so', [[<CMD>lua require('telescope.builtin').lsp_outgoing_calls()<CR>]])
     vim.keymap.set('n', '<Leader>ss', [[<CMD>lua require('telescope.builtin').lsp_workspace_symbols()<CR>]])
     vim.keymap.set('n', '<Leader>gi', [[<CMD>lua require('telescope.builtin').lsp_implementations()<CR>]])
     vim.keymap.set('n', '<Leader>gd', [[<CMD>lua require('telescope.builtin').lsp_definitions()<CR>]])
